ca06dec2db6880699fde8ad975202203fd19e373
deJames-13 <ail>
2025-02-19 18:08:23 +0800

    asdf

---
+++ mobile/states/api/resources.js
@@ -0,0 +1,21 @@
+import * as changeCase from "change-case";
+import resourceBuilder from '../base/viewsets';
+import { apiSlice } from './index';
+
+const resources = [
+    'users',
+];
+
+const customEndpoints = {
+
+}
+
+const resourceEndpoints = resources.reduce((acc, resource) => {
+    let name = changeCase.camelCase(resource);
+
+    return apiSlice.injectEndpoints({
+        endpoints: resourceBuilder(resource, customEndpoints[name]),
+    });
+}, {});
+
+export default resourceEndpoints;
