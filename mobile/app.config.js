8ef29c284a0a9ffd20c46c314b9d37b393e1ac64
deJames-13 <de.james013@gmail.com>
2025-03-15 17:49:16 +0800

    asd

---
+++ mobile/app.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+    expo: {
+        name: "EyeZone",
+        slug: "eyezone-mobile",
+        version: "1.0.0",
+        orientation: "portrait",
+        icon: "./assets/icon.png",
+        userInterfaceStyle: "light",
+        newArchEnabled: true,
+        splash: {
+            image: "./assets/splash-icon.png",
+            resizeMode: "contain",
+            backgroundColor: "#ffffff"
+        },
+        ios: {
+            supportsTablet: true
+        },
+        android: {
+            package: "com.eyezone.app",
+            adaptiveIcon: {
+                foregroundImage: "./assets/adaptive-icon.png",
+                backgroundColor: "#ffffff"
+            },
+            googleServicesFile: process.env.GOOGLE_SERVICES_JSON ?? "./google-services.json"
+        },
+        web: {
+            favicon: "./assets/favicon.png"
+        },
+        experiments: {
+            tsconfigPaths: true
+        },
+        plugins: [],
+        extra: {
+            eas: {
+                projectId: "851484ac-a9f8-4217-89af-b45f771a3953",
+            }
+        }
+    }
+};
