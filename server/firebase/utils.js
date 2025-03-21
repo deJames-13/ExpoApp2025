32504fa8144719b79c3f5a1db24e97b357dda31a
deJames-13 <de.james013@gmail.com>
2025-03-21 10:43:51 +0800

    ss

--- server/firebase/utils.js
+++ server/firebase/utils.js
@@ -3,11 +3,9 @@ import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-// Get the directory name using ES modules approach
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Read and parse the JSON file
 const serviceAccount = JSON.parse(
   fs.readFileSync(path.join(__dirname, './firebase-service.json'), 'utf8')
 );
